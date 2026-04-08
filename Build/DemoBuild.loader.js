671397bbb62dbd81ac6b88b1db92b101bda587c7
Jamie Till <j4mie.t1ll@gmail.com>
2026-04-08 16:17:03 +0100

    fixed misaligned button

--- Build/DemoBuild.loader.js
+++ Build/DemoBuild.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57271786,
+    wasmFileSize: 57271640,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
