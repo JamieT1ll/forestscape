e6052e6488050b73baf6488a11aef15faf2b4484
Jamie Till <j4mie.t1ll@gmail.com>
2026-04-14 18:37:34 +0100

    User friendly changes

--- Build/DemoBuild.loader.js
+++ Build/DemoBuild.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57271750,
+    wasmFileSize: 57342721,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
