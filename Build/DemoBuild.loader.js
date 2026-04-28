082a4dee1a195dea08e42aece361a30dc31f5525
Jamie Till <j4mie.t1ll@gmail.com>
2026-04-28 06:27:12 +0100

    Scene fix reupload

--- Build/DemoBuild.loader.js
+++ Build/DemoBuild.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57347277,
+    wasmFileSize: 57333708,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
