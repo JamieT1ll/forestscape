fa88c7c8f4b0431b8243d01ceb6716441f6d2e1b
Jamie Till <j4mie.t1ll@gmail.com>
2026-04-28 08:55:05 +0100

    Demobuild Upload

--- Build/DemoBuild.loader.js
+++ Build/DemoBuild.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57337836,
+    wasmFileSize: 57337600,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
