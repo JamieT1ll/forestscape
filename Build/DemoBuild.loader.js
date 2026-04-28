8e9838604027b622762bc3ecfe94a2b0025b4525
Jamie Till <j4mie.t1ll@gmail.com>
2026-04-28 04:22:51 +0100

    Update github with finished build- same as in submission

--- Build/DemoBuild.loader.js
+++ Build/DemoBuild.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57337008,
+    wasmFileSize: 57336846,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
